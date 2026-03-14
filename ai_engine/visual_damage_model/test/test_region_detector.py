b276c85a01c17bc332e346798984586d618fc63b
cunyie <kanyinsola.fagbayi@gmail.com>
2026-03-14 16:34:28 +0100

    Tested fusion engine with rag, ocr and visual

--- ai_engine/visual_damage_model/test/test_region_detector.py
+++
@@ -1,7 +0,0 @@
-from vision_engine.regions.region_detector import RegionDetector
-
-detector = RegionDetector()
-
-regions = detector.detect_regions("vision_dataset/images/train/kellogs_coco_pops_front.jpg")
-
-print(regions.keys())
